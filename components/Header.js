799e6451b98726a2b8423e688945a4e4842d9810
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2021-11-14 20:24:24 +0530

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
+import Avatar from "./Avatar";
+import HeaderOptions from "./HeaderOptions";
+
+const Header = () => {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png"
+          width={120}
+          height={40}
+          className="cursor-pointer"
+          onClick={() => router.push("/")}
+        />
+        <form className="flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="flex-grow w-full focus:outline-none"
+          />
+          <XIcon
+            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+            onClick={() => (searchInputRef.current.value = "")}
+          />
+          <MicrophoneIcon className="h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+          <SearchIcon
+            className="h-6 text-blue-500 hidden sm:inline-flex cursor-pointer"
+            onClick={search}
+          />
+          <button hidden type="submit" onClick={search}>
+            Search
+          </button>
+        </form>
+        <Avatar
+          className="ml-auto"
+          url="https://pbs.twimg.com/profile_images/1292721383546318850/KU8pErTW_400x400.jpg"
+        />
+      </div>
+
+      <HeaderOptions />
+    </header>
+  );
+};
+
+export default Header;
