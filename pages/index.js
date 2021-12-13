2033452097b83fdc4c8c20c964849b94269c2e4b
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2021-12-13 18:29:42 +0530

    Apostrophe fixed

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
           </button>
 
           <button onClick={search} className="btn">
-            I'm Feeling Lucky
+            I&apos;m Feeling Lucky
           </button>
         </div>
       </form>
